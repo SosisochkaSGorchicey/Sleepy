12d6c7409a9a4e4575496b76be404d7d8671fbe3
sosisochka <kudrina.alice@yandex.ru>
2024-07-02 23:12:04 +0300

    Code cleanup

--- feature/player/src/main/kotlin/com/feature/player/PlayerScreen.kt
+++ feature/player/src/main/kotlin/com/feature/player/PlayerScreen.kt
@@ -1,12 +1,11 @@
 package com.feature.player
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
 
-class PlayerScreen: Screen {
+class PlayerScreen : Screen {
     @Composable
     override fun Content() {
-        Text(text = "mnjhbg")
+
     }
 }
