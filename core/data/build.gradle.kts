16f1333e3365389a65e6ad08a38493725555d6a7
Alisa <alicekjob@gmail.com>
2024-08-15 21:39:14 +0300

    Added buildConfig

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,5 +1,23 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+import java.io.FileInputStream
+
 plugins {
     alias(libs.plugins.convetion.library)
 }
 
 android.namespace = "com.core.data"
+
+private val keystorePropertiesFile = file("keys.properties")
+private val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+android{
+    defaultConfig {
+        buildConfigField("String", "SUPABASE_URL", keystoreProperties.getProperty("supabaseUrl"))
+        buildConfigField("String", "SUPABASE_SECRET_KEY", keystoreProperties.getProperty("supabaseSecretKey"))
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
