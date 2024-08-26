9236edd10395fadba7b8264c6776c29965c49b71
sosisochka <kudrina.alice@yandex.ru>
2024-08-26 21:08:49 +0300

    for test

--- feature/content/src/main/kotlin/com/feature/content/presentation/ContentScreen.kt
+++ feature/content/src/main/kotlin/com/feature/content/presentation/ContentScreen.kt
@@ -1,14 +1,27 @@
 package com.feature.content.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
+import com.core.ui.uiElements.mainScreenElements.MainBottomBar
 
 object ContentScreen : Screen {
     @Composable
     override fun Content() {
 
-        Text(text = "ContentScreen")
+        Scaffold(
+            bottomBar = {
+                MainBottomBar()
+            }
+        ) {
+            Column {
+
+                Text(text = "ContentScreen")
+            }
+        }
+
 
     }
 }
