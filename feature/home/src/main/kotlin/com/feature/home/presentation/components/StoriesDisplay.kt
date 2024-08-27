0178c671d917c672c48b8a2aaa6c195b4da96ae4
sosisochka <kudrina.alice@yandex.ru>
2024-08-27 23:09:06 +0300

    Made story clickable

--- feature/home/src/main/kotlin/com/feature/home/presentation/components/StoriesDisplay.kt
+++ feature/home/src/main/kotlin/com/feature/home/presentation/components/StoriesDisplay.kt
@@ -2,6 +2,7 @@ package com.feature.home.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
@@ -38,6 +39,7 @@ fun StoriesDisplay() {
                         shape = AppTheme.shapes.smallestCornersDp,
                         color = AppTheme.colors.basePeachy
                     )
+                    .clickable { }
 
             )
         }
